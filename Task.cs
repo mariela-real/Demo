180ff2847b3fd716f88f4b19d211e8463a3cb5c1
mariela-real <fernandezrealmariela@gmail.com>
2024-01-06 10:42:43 -0400

    primera historia de usuario

---
+++ Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Demo
+{
+    public class Task
+    {
+        
+        public string Id { get; set; }
+        
+        public int TimeEstimatedTime { get; set; }
+        
+        public string ActualState { get; set; }
+        
+        public string Description { get; set; }
+   
+
+        public Task(string id, int timeEstimatedTime, string actualState, string description) 
+        {
+            Id = id;
+            TimeEstimatedTime = timeEstimatedTime;
+            ActualState = actualState;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+
+            return "Tareas: " + Id + " " + TimeEstimatedTime + "" + ActualState + "" + Description;
+        }
+    }
+}
