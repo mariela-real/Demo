267c01f05e0dcd803c80c1f58e002913300e5e63
mariela-real <fernandezrealmariela@gmail.com>
2024-01-07 17:43:31 -0400

    historia de usuario 2

--- Program.cs
+++ Program.cs
@@ -1,41 +1,60 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace Demo 
-{
+namespace Demo
+{ 
     public class Program
     {
         public static void Main(string[] args)
         {
+            List<Task> tasks = new List<Task>()
+            {
+                new Task(1, 2, "pending", "Hacer la primera hu para la truchita"),
+                new Task(2, 3, "Completed", "end"),
+            };
 
-            List<Task> task = new List<Task>()
+            Person person_1 = new Person
             {
-                new Task("1", 2, "pending", "Hacer la primera hu para la truchita"),
+                Id = 1,
+                Name = "Ruben",
+                Age = 23,
+                Location = "Av. 123",
             };
-           // var task = new List<Task> { new Task("1", 2, "pending", "Hacer la primera hu para la truchita"), };
-            var result = new User
+          
+            var user_1 = new User
             {
-                Id = "1",
-                Name = "Mariela",
-                Tasks = task,
+                Id = 1,
+                Person = person_1,
+                Role = Role.Admin,
+                Tasks = tasks,
             };
-           // Console.WriteLine(result );
-            PrintUser(result);
+           
+            PrintUser(user_1);
         }
-        public static void PrintUser(User persona)
+        public static void PrintUser(User userEntity)
         {
+            UserDTO userDTO = new UserDTO
+            {
+                Id = userEntity.Id,
+                Name = userEntity.Person!.Name,
+                Role = userEntity.Role,
+                Tasks = userEntity.Tasks
+            };
+
             Console.WriteLine(
-                $"Persona ID: {persona.Id}" +
-                $"Nombre: {persona.Name}"
+                $"ID: {userDTO.Id} \n" +
+                $"Nombre: {userDTO.Name} \n" +
+                $"Rol: {userDTO.Role} \n" 
+      
             );
 
-            foreach (var tarea in persona.Tasks)
+            foreach (var task in userDTO.Tasks)
             {
-                Console.WriteLine($"Tarea ID: {tarea.Id}" +
-                     $"Hora: {tarea.TimeEstimatedTime}" +
-                     $"Estado actual: {tarea.ActualState}" +
-                    $"descripcion: {tarea.Description}"
-                   
-                    );
+                Console.WriteLine($"Tarea ID: {task.Id} \n" +
+                    $"Hora: {task.Estimation} \n" +
+                    $"Estado actual: {task.ActualState} \n" +
+                    $"descripcion: {task.Description} \n"
+                );
             }
         }
 
--- obj/Debug/net8.0/Demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d93c814438dc0965a2721dbc16bd2a7f8837439c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
